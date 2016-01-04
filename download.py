01e021c4cc15cc24ee45373a7e07fac061f7f1d0
everm1nd <everm1nd.mail@gmail.com>
2016-01-04 15:18:53 +0100

    removed duplicate for #download_celeb_a

--- download.py
+++ download.py
@@ -60,20 +60,6 @@ def unzip(filepath):
 def download_celeb_a(dirpath):
     data_dir = 'celebA'
     if os.path.exists(os.path.join(dirpath, data_dir)):
-        print('Found Stanford POS Tagger - skip')
-        return
-    url = 'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADVdnYbokd7TXhpvfWLL3sga/img_align_celeba.zip?dl=1'
-    filepath = download(url, dirpath)
-    zip_dir = ''
-    with zipfile.ZipFile(filepath) as zf:
-        zip_dir = zf.namelist()[0]
-        zf.extractall(dirpath)
-    os.remove(filepath)
-    os.rename(os.path.join(dirpath, zip_dir), os.path.join(dirpath, data_dir))
-
-def download_celeb_a(dirpath):
-    data_dir = 'celebA'
-    if os.path.exists(os.path.join(dirpath, data_dir)):
         print('Found Celeb-A - skip')
         return
     url = 'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADVdnYbokd7TXhpvfWLL3sga/img_align_celeba.zip?dl=1'
