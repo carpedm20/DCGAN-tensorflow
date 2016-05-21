change losses to scalar and batch normalization. fix #10, #17.

main.py
@@ -17,6 +17,7 @@ flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the 
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
 flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("is_crop", False, "True for training, False for testing [False]")
+flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 FLAGS = flags.FLAGS
 
 def main(_):
@@ -40,15 +41,16 @@ def main(_):
         else:
             dcgan.load(FLAGS.checkpoint_dir)
 
-        to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
-                                      [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
-                                      [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
-                                      [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
-                                      [dcgan.h4_w, dcgan.h4_b, None])
+        if FLAGS.visualize:
+            to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
+                                          [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
+                                          [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
+                                          [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
+                                          [dcgan.h4_w, dcgan.h4_b, None])
 
-        # Below is codes for visualization
-        OPTION = 2
-        visualize(sess, dcgan, FLAGS, OPTION)
+            # Below is codes for visualization
+            OPTION = 2
+            visualize(sess, dcgan, FLAGS, OPTION)
 
 if __name__ == '__main__':
     tf.app.run()

model.py
@@ -44,18 +44,18 @@ class DCGAN(object):
         self.c_dim = 3
 
         # batch normalization : deals with poor initialization helps gradient flow
-        self.d_bn1 = batch_norm(batch_size, name='d_bn1')
-        self.d_bn2 = batch_norm(batch_size, name='d_bn2')
+        self.d_bn1 = batch_norm(name='d_bn1')
+        self.d_bn2 = batch_norm(name='d_bn2')
 
         if not self.y_dim:
-            self.d_bn3 = batch_norm(batch_size, name='d_bn3')
+            self.d_bn3 = batch_norm(name='d_bn3')
 
-        self.g_bn0 = batch_norm(batch_size, name='g_bn0')
-        self.g_bn1 = batch_norm(batch_size, name='g_bn1')
-        self.g_bn2 = batch_norm(batch_size, name='g_bn2')
+        self.g_bn0 = batch_norm(name='g_bn0')
+        self.g_bn1 = batch_norm(name='g_bn1')
+        self.g_bn2 = batch_norm(name='g_bn2')
 
         if not self.y_dim:
-            self.g_bn3 = batch_norm(batch_size, name='g_bn3')
+            self.g_bn3 = batch_norm(name='g_bn3')
 
         self.dataset_name = dataset_name
         self.checkpoint_dir = checkpoint_dir
@@ -84,9 +84,9 @@ class DCGAN(object):
         self.d__sum = tf.histogram_summary("d_", self.D_)
         self.G_sum = tf.image_summary("G", self.G)
 
-        self.d_loss_real = tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D))
-        self.d_loss_fake = tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_))
-        self.g_loss = tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_))
+        self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
+        self.d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
+        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
 
         self.d_loss_real_sum = tf.scalar_summary("d_loss_real", self.d_loss_real)
         self.d_loss_fake_sum = tf.scalar_summary("d_loss_fake", self.d_loss_fake)

ops.py
@@ -8,29 +8,37 @@ from utils import *
 
 class batch_norm(object):
     """Code modification of http://stackoverflow.com/a/33950177"""
-    def __init__(self, batch_size, epsilon=1e-5, momentum = 0.1, name="batch_norm"):
-        with tf.variable_scope(name) as scope:
+    def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
+        with tf.variable_scope(name):
             self.epsilon = epsilon
             self.momentum = momentum
-            self.batch_size = batch_size
 
             self.ema = tf.train.ExponentialMovingAverage(decay=self.momentum)
-            self.name=name
+            self.name = name
 
     def __call__(self, x, train=True):
         shape = x.get_shape().as_list()
 
-        with tf.variable_scope(self.name) as scope:
-            self.gamma = tf.get_variable("gamma", [shape[-1]],
-                                initializer=tf.random_normal_initializer(1., 0.02))
-            self.beta = tf.get_variable("beta", [shape[-1]],
-                                initializer=tf.constant_initializer(0.))
+        if train:
+            with tf.variable_scope(self.name) as scope:
+                self.beta = tf.get_variable("beta", [shape[-1]],
+                                    initializer=tf.constant_initializer(0.))
+                self.gamma = tf.get_variable("gamma", [shape[-1]],
+                                    initializer=tf.random_normal_initializer(1., 0.02))
 
-            self.mean, self.variance = tf.nn.moments(x, [0, 1, 2])
+                batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+                ema_apply_op = self.ema.apply([batch_mean, batch_var])
+                self.ema_mean, self.ema_var = self.ema.average(batch_mean), self.ema.average(batch_var)
 
-            return tf.nn.batch_norm_with_global_normalization(
-                x, self.mean, self.variance, self.beta, self.gamma, self.epsilon,
-                scale_after_normalization=True)
+                with tf.control_dependencies([ema_apply_op]):
+                    mean, var = tf.identity(batch_mean), tf.identity(batch_var)
+        else:
+            mean, var = self.ema_mean, self.ema_var
+
+        normed = tf.nn.batch_norm_with_global_normalization(
+                x, mean, var, self.beta, self.gamma, self.epsilon, scale_after_normalization=True)
+
+        return normed
 
 def binary_cross_entropy(preds, targets, name=None):
     """Computes binary cross entropy given `preds`.