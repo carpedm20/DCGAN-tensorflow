b7429b63929d0be209c4f505a8456f3e3c7ddd97
Taehoon Kim <carpedm20@gmail.com>
2016-11-14 10:49:03 +0000

    omit to_json

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def main(_):
             dcgan.load(FLAGS.checkpoint_dir)
 
         if FLAGS.visualize:
-            to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
-                                          [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
-                                          [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
-                                          [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
-                                          [dcgan.h4_w, dcgan.h4_b, None])
+            # to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
+            #                               [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
+            #                               [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
+            #                               [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
+            #                               [dcgan.h4_w, dcgan.h4_b, None])
 
             # Below is codes for visualization
             OPTION = 2
