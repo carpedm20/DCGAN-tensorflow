e8f3dabeae0e548a69ee9bc6108dcfe3fbfb62ff
Taehoon Kim <carpedm20@gmail.com>
2016-12-20 17:28:33 +0900

    dealing with TF legacy. Fix #69

--- ops.py
+++ ops.py
@@ -6,6 +6,18 @@ from tensorflow.python.framework import ops
 
 from utils import *
 
+try:
+    image_summary = tf.image_summary
+    scalar_summary = tf.scalar_summary
+    histogram_summary = tf.histogram_summary
+    merge_summary = tf.merge_summary
+    SummaryWriter = tf.train.SummaryWriter
+except:
+    image_summary = tf.summary.image
+    scalar_summary = tf.summary.scalar
+    histogram_summary = tf.summary.histogram
+    merge_summary = tf.summary.merge
+    SummaryWriter = tf.summary.FileWriter
 
 class batch_norm(object):
     def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
