84745a0e9a8d4d89601e79222c06415e927d56fd
Evgeniy Zheltonozhskiy <zheltonozhskiy@gmail.com>
2017-02-07 20:07:22 +0200

    Add concat to support TF 1.0

--- ops.py
+++ ops.py
@@ -39,7 +39,11 @@ def conv_cond_concat(x, y):
   """Concatenate conditioning vector on feature map axis."""
   x_shapes = x.get_shape()
   y_shapes = y.get_shape()
-  return tf.concat_v2([
+  try:
+    return tf.concat_v2([
+      x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
+  except:
+    return tf.concat([
       x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
 
 def conv2d(input_, output_dim, 
