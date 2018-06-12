1b09bf88b3627227810ff1bb6888c7c2ad47ef6c
spacemunkay <jasondenney@gmail.com>
2018-06-12 13:42:35 -0700

    Raise err if dataset size less than batch_size

--- model.py
+++ model.py
@@ -85,6 +85,9 @@ class DCGAN(object):
 
     self.grayscale = (self.c_dim == 1)
 
+    if len(self.data) < self.batch_size:
+      raise Exception("[!] Entire dataset size is less than the configured batch_size")
+
     self.build_model()
 
   def build_model(self):
